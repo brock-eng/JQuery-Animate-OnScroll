26a4ab667a94715b94ebd0c6244d8a460e2ba433
brock-eng <85470716+brock-eng@users.noreply.github.com>
2021-10-12 13:50:45 -0500

    Update animate.js

--- animate.js
+++ animate.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
     $(window).on("load",function() {
         $(window).scroll(function() {
             var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-            $(".logos").each(function() {
+            $(".to-animate").each(function() {
                 /* Check the location of each desired element */
                 var objectBottom = $(this).offset().top + $(this).outerHeight();
                 
                 if (objectBottom < windowBottom) {
-                    $(this).addClass("slide-in");
+                    $(this).addClass("animation-effect");
                 }
             });
         }).scroll(); //invoke scroll-handler on page-load
