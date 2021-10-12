bdf2871609fc79acd4e843fe55804d4f509593d0
brock-eng <85470716+brock-eng@users.noreply.github.com>
2021-10-12 14:12:00 -0500

    Update animate.js

--- animate.js
+++ animate.js
@@ -1,17 +1,27 @@
-$(document).ready(function() {
-
-    $(window).on("load",function() {
-        $(window).scroll(function() {
-            var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-            $(".to-animate").each(function() {
-                /* Check the location of each desired element */
-                var objectBottom = $(this).offset().top + $(this).outerHeight();
-                
-                if (objectBottom < windowBottom) {
-                    $(this).addClass("animation-effect");
-                }
-            });
-        }).scroll(); //invoke scroll-handler on page-load
+/**
+ * Applies a specified animation class to a target classes on scroll reveal.
+ * @author Zach Brock
+ * @param {string} targetClass           Class to be animated.
+ * @param {string} animationClass        Animation class to be applied on scroll.
+ */
+function Animate(targetClass, animationClass) {
+    jQuery(function($) {
+        $(window).on("load",function() {
+            $(window).scroll(function() {
+                var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+                $(targetClass).each(function() {
+                    /* Check the location of each desired element */
+                    var objectBottom = $(this).offset().top + $(this).outerHeight();
+                    
+                    if (objectBottom < windowBottom) {
+                        $(this).addClass(animationClass);
+                    }
+                });
+            }).scroll(); //invoke scroll-handler on page-load
+        });
     });
+};
 
+$(document).ready(function() {
+    Animate(".logos", "slide-in")
 });
