c12474c3dbdc694831bfb3eb7c799f204b523d37
Zach Brock <85470716+brock-eng@users.noreply.github.com>
2021-10-19 08:39:18 -0500

    Update animate.js

--- animate.js
+++ animate.js
@@ -1,20 +1,24 @@
 /**
- * Applies a specified animation class to a target class on scroll reveal.
- * @author Zach Brock
+ * Applies a specified animation class to a target classes on scroll reveal.
+ * @author Zach Brock -> https://github.com/brock-eng/JQuery-Animate-OnScroll/edit/main/animate.js
  * @param {string} targetClass           Class to be animated.
  * @param {string} animationClass        Animation class to be applied on scroll.
+ * @param {boolean} resetOnScrollUp      Set to true to reset the animation when scrolling back up the page
  */
-function Animate(targetClass, animationClass) {
+function AnimateOnScroll(targetClass, animationClass, resetOnScrollUp) {
+    targetClass = "." + targetClass;
     jQuery(function($) {
         $(window).on("load",function() {
             $(window).scroll(function() {
                 var windowBottom = $(this).scrollTop() + $(this).innerHeight();
                 $(targetClass).each(function() {
                     /* Check the location of each desired element */
-                    var objectBottom = $(this).offset().top + $(this).outerHeight();
+                    var objectBottom = $(this).offset().top;
                     
                     if (objectBottom < windowBottom) {
                         $(this).addClass(animationClass);
+                    } else if (resetOnScrollUp) {
+                        $(this).removeClass(animationClass);
                     }
                 });
             }).scroll(); //invoke scroll-handler on page-load
@@ -23,7 +27,8 @@ function Animate(targetClass, animationClass) {
 };
 
 
-// Sample call implementation
+// Sample usage.  Replace classes with your own CSS animations
 $(document).ready(function() {
-    Animate(".logos", "slide-in");
+    AnimateOnScroll("logos", "slide-in", true)
 });
+
